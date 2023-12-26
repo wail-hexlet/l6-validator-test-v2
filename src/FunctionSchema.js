ac2c5c1b8cb70ab0e0e3fce56856ec48704179c1
Anatoliy Tupitsin <hexlet@wailbox.ru>
2023-12-26 21:58:15 +0300

    task 4

--- src/FunctionSchema.js
+++ src/FunctionSchema.js
@@ -1,24 +1,27 @@
 export default class FunctionSchema {
-    constructor(validators, context = {}, expectedValue = null) {
+    constructor(validators, context = {}, expectedValue = null, args = []) {
       this.validators = validators;
       this.context = context;
       this.expectedValue = expectedValue;
-
+      this.args = args;
     }
   
     isValid(value) {
       if (!this.expectedValue) {
         return this.validators.every((validator) => validator(value) === true);
       }
-      return value.call(this.context,) === this.expectedValue;
+      return value.call(this.context, ...this.args) === this.expectedValue;
     }
   
     expect(value) {
-      return new FunctionSchema(this.validators, this.context, value);
+      return new FunctionSchema(this.validators, this.context, value, this.args);
     }
   
     callWith(context) {
-      return new FunctionSchema(this.validators, context, this.expectedValue,);
+      return new FunctionSchema(this.validators, context, this.expectedValue, this.args);
     }
   
+    arguments(...args) {
+      return new FunctionSchema(this.validators, this.context, this.expectedValue, args);
+    }
   }
